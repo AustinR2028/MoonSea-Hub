600cba495d97f9acb5909938e5437af9c80dd3e6
AustinR2028 <86375641+AustinR2028@users.noreply.github.com>
2023-03-01 18:04:09 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -31,6 +31,19 @@ Counter:Button{
 	Name = "Count Tool Instances",
 	Description = "Counts the item under counted item",
 	Callback = function() 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/AustinR2028/MoonSea-Hub/main/Buttons/counter.lua"))()
+	local plr = game:getService("Players").LocalPlayer
+	local backpack = plr.Backpack
+	local Items = backpack:getChildren()
+	local Tools = table.getn(Items)
+	local num = 0
+	local found = 0
+	while num ~= Tools do
+ 	 num = num+1
+	  local focus = Items[num].Name
+	  if focus == search then
+ 	   found = found+1
+ 	 end
+	end
+	print(found)
 	end
 }
